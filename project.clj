52908ad72ad8b3459f42e0f14a21bdafcc8b4ec7
Tommi Reiman <tommi@iki.fi>
2014-04-04 13:48:53 +0300

    bump up version for signed jars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/fnhouse-swagger "0.1.0"
+(defproject metosin/fnhouse-swagger "0.1.1"
   :description "Swagger-support for fnhouse"
   :url "https://github.com/metosin/fnhouse-swagger"
   :license {:name "Eclipse Public License"
