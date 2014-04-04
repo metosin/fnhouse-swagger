2803774cb5d532eaa7e9510dc41e8c2b46a1d241
Tommi Reiman <tommi@iki.fi>
2014-04-04 13:52:11 +0300

    update dependencies

--- examples/guesthouse/project.clj
+++ examples/guesthouse/project.clj
@@ -3,13 +3,13 @@
   :url "https://github.com/Prismatic/fnhouse/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[prismatic/plumbing "0.2.1"]
+  :dependencies [[prismatic/plumbing "0.2.2"]
                  [prismatic/fnhouse "0.1.0"]
-                 [metosin/fnhouse-swagger "0.1.0"]
+                 [metosin/fnhouse-swagger "0.1.1"]
                  [metosin/ring-swagger "0.8.3"]
                  [metosin/ring-swagger-ui "2.0.12-1"]
                  [org.clojure/clojure "1.5.1"]
-                 [clj-http "0.9.0"]
-                 [ring/ring-core "1.0.0-RC1"]
-                 [ring/ring-jetty-adapter "1.0.0-RC1"]
-                 [ring/ring-json "0.2.0"]])
+                 [clj-http "0.9.1"]
+                 [ring/ring-core "1.2.2"]
+                 [ring/ring-jetty-adapter "1.2.2"]
+                 [ring/ring-json "0.3.0"]])
