afa4a5c80c9cb71b8cafd90e49ada26d95e6cb54
Tommi Reiman <tommi@iki.fi>
2015-01-05 22:06:51 +0200

    merged 1.2 extension points in.

--- src/fnhouse/swagger.clj
+++ src/fnhouse/swagger.clj
@@ -24,29 +24,44 @@
 (defn- ignore-ns? [ns-sym]
   (:no-doc (meta (the-ns ns-sym))))
 
-(defn- collect-route [ns-sym->prefix api-routes annotated-handler]
-  (letk [[[:info method path description request responses
+(defn- collect-route [ns-sym->prefix extra-metadata-fn api-routes annotated-handler]
+  (letk [[[:info method path description request responses annotations
            [:source-map ns]]] annotated-handler]
     (let [ns-sym (symbol ns)
-          prefix (ns-sym->prefix ns-sym)]
+          prefix (ns-sym->prefix ns-sym)
+          extra-metadata (or (extra-metadata-fn annotations) {})]
       (if (ignore-ns? ns-sym)
         api-routes
         (assoc-in api-routes [path method]
-                  {:tags [prefix]
-                   :summary description
-                   :description description
-                   :responses (convert-responses responses)
-                   :parameters (convert-parameters request)})))))
+                  (merge extra-metadata
+                         {:tags        [prefix]
+                          :summary     description
+                          :description description
+                          :responses   (convert-responses responses)
+                          :parameters  (convert-parameters request)}))))))
 
 ;;
 ;; Public API
 ;;
 
 (defn collect-routes
-  ([handlers prefix->ns-sym] (collect-routes handlers prefix->ns-sym {}))
+  "Parameters:
+  - seq of fnhouse AnnotatedProtoHandlers
+  - prefix->ns-sym map
+  - top-level base swagger extra parameters, defaults to {}
+  - extra function (like fnhouse extra-info-fn). It takes contents of :annotations
+  field on handler and returns a map that will be merged into
+  ring-swagger's Operation-data. Such function can be used to obtain Swagger auth
+  spec from fnhouse's handler or to override any fnhouse-swagger-derived
+  metadata."
+  ([handlers prefix->ns-sym]
+    (collect-routes handlers prefix->ns-sym {}))
   ([handlers prefix->ns-sym base]
+    (collect-routes handlers prefix->ns-sym base (constantly {})))
+  ([handlers prefix->ns-sym base extra-metadata-fn]
     (let [ns-sym->prefix (map-invert prefix->ns-sym)
-          api-routes (reduce (partial collect-route ns-sym->prefix) {} handlers)]
+          reducer (partial collect-route ns-sym->prefix extra-metadata-fn)
+          api-routes (reduce reducer {} handlers)]
       (assoc base :paths api-routes))))
 
 (def wrap-swagger-ui swagger-ui/wrap-swagger-ui)
