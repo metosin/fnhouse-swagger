7b2b2435744d4ecba9dc90b5d95e2ef07eb493c8
Tommi Reiman <tommi@iki.fi>
2015-01-05 11:01:11 +0200

    update deps

--- examples/guesthouse/project.clj
+++ examples/guesthouse/project.clj
@@ -1,14 +1,14 @@
-(defproject guesthouse "0.1.0-SNAPSHOT"
-  :description "Example guestbook project for demonstrating fnhouse"
+(defproject guesthouse "0.2.0"
+  :description "Example guestbook project for demonstrating fnhouse & swagger"
   :url "https://github.com/Prismatic/fnhouse/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[prismatic/plumbing "0.3.5"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [prismatic/plumbing "0.3.5"]
                  [prismatic/fnhouse "0.1.1"]
-                 [metosin/fnhouse-swagger "0.4.0" :exclusions [org.clojure/clojure]]
-                 [metosin/ring-swagger "0.15.0" :exclusions [org.clojure/clojure]]
+                 [metosin/fnhouse-swagger "0.5.0-SNAPSHOT" :exclusions [org.clojure/clojure]]
+                 [metosin/ring-swagger "0.16.0-SNAPSHOT" :exclusions [org.clojure/clojure]]
                  [metosin/ring-swagger-ui "2.0.24"]
-                 [org.clojure/clojure "1.6.0"]
                  [clj-http "1.0.1"]
                  [ring/ring-core "1.3.2"]
                  [ring/ring-jetty-adapter "1.3.2"]
