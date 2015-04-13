a26b1e14119d2e8b3a53681a92d51010517c5a8e
Tommi Reiman <tommi@iki.fi>
2015-04-13 23:40:13 +0300

    bump up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/fnhouse-swagger "0.5.0"
+(defproject metosin/fnhouse-swagger "0.5.1"
   :description "Swagger-support for fnhouse"
   :url "https://github.com/metosin/fnhouse-swagger"
   :license {:name "Eclipse Public License"
