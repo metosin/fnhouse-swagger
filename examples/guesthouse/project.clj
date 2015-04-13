dc98ab3f2fc77efd8ad35a415ed1506e57fce7cd
Tommi Reiman <tommi@iki.fi>
2015-04-13 23:43:21 +0300

    update guesthouse

--- examples/guesthouse/project.clj
+++ examples/guesthouse/project.clj
@@ -4,8 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [prismatic/plumbing "0.3.5"]
-                 [metosin/fnhouse-swagger "0.5.0"]
+                 [metosin/fnhouse-swagger "0.5.1"]
                  [metosin/ring-swagger-ui "2.1.0-alpha.6-SNAPSHOT"]
                  [clj-http "1.0.1"]
                  [ring/ring-core "1.3.2"]
