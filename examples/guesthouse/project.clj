69cdd92a5bb6466dce0c35e369147d7a2b598533
Tommi Reiman <tommi@iki.fi>
2015-04-26 21:55:52 +0300

    update versions

--- examples/guesthouse/project.clj
+++ examples/guesthouse/project.clj
@@ -1,12 +1,12 @@
-(defproject guesthouse "0.3.0"
+(defproject guesthouse "0.3.1"
   :description "Example guestbook project for demonstrating fnhouse & swagger"
   :url "https://github.com/Prismatic/fnhouse/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [metosin/fnhouse-swagger "0.5.1"]
-                 [metosin/ring-swagger-ui "2.1.0-alpha.6-SNAPSHOT"]
-                 [clj-http "1.1.0"]
+                 [metosin/fnhouse-swagger "0.5.2"]
+                 [metosin/ring-swagger-ui "2.1.1-M2"]
+                 [clj-http "1.1.1"]
                  [ring/ring-core "1.3.2"]
                  [ring/ring-jetty-adapter "1.3.2"]
                  [ring/ring-json "0.3.1"]])
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/fnhouse-swagger "0.5.1"
+(defproject metosin/fnhouse-swagger "0.5.2"
   :description "Swagger-support for fnhouse"
   :url "https://github.com/metosin/fnhouse-swagger"
   :license {:name "Eclipse Public License"
@@ -6,12 +6,12 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [prismatic/plumbing "0.4.2"]
                  [prismatic/fnhouse "0.1.1"]
-                 [metosin/ring-swagger "0.19.5"]
+                 [metosin/ring-swagger "0.20.1"]
                  [ring/ring-core "1.3.2"]]
   :profiles {:dev {:plugins [[lein-clojars "0.9.1"]
                              [lein-midje "3.1.3"]]
                    :dependencies [[midje "1.7.0-SNAPSHOT"]
-                                  [metosin/ring-swagger-ui "2.0.24"]]}
+                                  [metosin/ring-swagger-ui "2.1.1-M2"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha4"]]}}
   :aliases {"all" ["with-profile" "dev:dev,1.7"]
             "test-ancient" ["midje"]})
